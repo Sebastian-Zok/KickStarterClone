b3b2bf1f9b0430d1df56b5f4c427002848c966a9
Sebastian-Zok <sebastianpaulzok@gmail.com>
2023-01-02 22:59:08 +0100

    Multi-Project-Setup

---
+++ 0-kickstarterClone-plugin-database/src/main/java/com/sebastianZok/Main.java
@@ -0,0 +1,13 @@
+package com.sebastianZok;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( 'This is your command line based crowdfunding platform!');
+    }
+}
